8895af778f24dc21ddf442d8d68d6bfa0e17806c
Allan <allan.vinicius@parkplusionc.com>
2025-05-01 17:18:46 -0300

    Bugs

--- Models/Estoque.cs
+++ Models/Estoque.cs
@@ -6,9 +6,12 @@ public class Estoque
     public int ProdutoId { get; set; } // Referência ao produto
     public Produto Produto { get; set; } = new(); // Navegação para a entidade Produto
     public int Quantidade { get; set; } // Quantidade disponível no estoque
+    public int? QuantidadeMinima { get; set; }
     public DateTime DataEntrada { get; set; } // Data de entrada do produto no estoque
     public DateTime? DataValidade { get; set; } // Data de validade (se aplicável)
     public decimal? PrecoCusto { get; set; } // Preço de custo do produto no estoque
+    public int? Margem { get; set; }
+    public decimal? PrecoVenda { get; set; } // Preço de venda do produto no estoque
     public string? LocalArmazenamento { get; set; } // Local onde o produto está armazenado
     public bool Ativo { get; set; } = true; // Indica se o item está ativo no estoque
 }

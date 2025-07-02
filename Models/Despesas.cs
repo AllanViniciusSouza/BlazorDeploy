ef20e800acea53da0ae517b8eba5c56cb81d20cc
Allan <allan.vinicius@parkplusionc.com>
2025-07-02 16:45:11 -0300

    despesas page 2

--- Models/Despesas.cs
+++ Models/Despesas.cs
@@ -4,6 +4,7 @@ namespace BlazorDeploy.Models
 {
     public class Despesas
     {
+        public int Id { get; set; }
         [DataType(DataType.Date)]
         public DateTime DataSelecionada { get; set; } = DateTime.Today;
         public string Descricao { get; set; } = string.Empty;
@@ -11,7 +12,7 @@ namespace BlazorDeploy.Models
 
         public string FormaPagamento { get; set; } = string.Empty;
 
-        public decimal Valor { get; set; };
+        public decimal Valor { get; set; }
 
         public int Parcelas{ get; set; }
 

6c4b77c063d9cccc8524daad7ed815c831d1b092
Allan <allan.vinicius@parkplusionc.com>
2025-07-11 12:09:09 -0300

    buguizinho

--- Models/Despesas.cs
+++ Models/Despesas.cs
@@ -5,8 +5,7 @@ namespace BlazorDeploy.Models
     public class Despesas
     {
         public int Id { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime DataSelecionada { get; set; } = DateTime.Today;
+        public DateTime Data { get; set; } = DateTime.Today;
         public string Descricao { get; set; } = string.Empty;
         public string Categoria { get; set; } = string.Empty;
 
@@ -16,7 +15,8 @@ namespace BlazorDeploy.Models
 
         public int Parcelas{ get; set; }
 
-        public string? Observação { get; set; } = string.Empty;
+        public string? Observacao { get; set; } = string.Empty;
+        public int UsuarioId { get; set; } = 1;
     }
 
 }
